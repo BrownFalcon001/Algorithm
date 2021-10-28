e919c2b1024963af14559d919bc84de669b3a4a8
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2021-10-28 14:57:00 +0600

    Update EA_G.cpp

--- Math/Euclidean Algo/General/EA_G.cpp
+++ Math/Euclidean Algo/General/EA_G.cpp
@@ -1,6 +1,8 @@
 //there are another implemented algo which done the same stuff iteratively
 //you will find that in the same folder
 
+///time complexity = O(log(min(a, b)))
+
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
