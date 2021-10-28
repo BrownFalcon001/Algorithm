e6efc41da3e586d64cb39094c36518726a01399b
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2021-10-28 12:06:17 +0600

    Create BE_iterative.cpp

---
+++ Math/Binary Exponentiation/BE_iterative.cpp
@@ -0,0 +1,20 @@
+//iterative approach
+// it is way faster than recursive approach
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int power(int a, int b) {
+    int result = 1;
+    while(b) {
+        if(b%2) result *=a;
+        a *=a;
+        b /=2;
+    }
+    return result;
+}
+
+int main() {
+    int a, b;cin>>a>>b;
+    cout<<power(a,b)<<endl;
+}
