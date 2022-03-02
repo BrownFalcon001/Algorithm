0015a9fd5866aa51c7df0f7c8c5dca17c2561e65
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2022-03-02 14:56:50 +0600

    Create o1_ME_recursive.cpp

---
+++ Math/Matrix Exponentiation/o1_ME_recursive.cpp
@@ -0,0 +1,72 @@
+/*
+* Author: BrownFalcon
+*/
+
+//Time Complextiy: O(k^3 * log n) ; where we have a k*k matrix
+//Space Complextiy: O(k^3 * log n)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct matrix {
+    int mat[2][2];
+};
+
+matrix mul(matrix A, matrix B) {
+    matrix C;
+    for(int row = 0; row<2; row++) {
+        for(int col = 0; col<2; col++) {
+            int x = 0;
+            for(int i=0;i<2;i++) {
+                x+= A.mat[row][i] * B.mat[i][col];
+            }
+            C.mat[row][col] = x;
+        }
+    }
+    return C;
+}
+
+matrix identity() {
+    matrix C;
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<2;j++) {
+            if(i==j) C.mat[i][j] = 1;
+            else C.mat[i][j] = 0;
+        }
+    }
+    return C;
+}
+
+matrix power(matrix A, int n) {
+    if(n==0) return identity();
+    matrix temp = power(A, n/2);
+    if(n%2==0) {
+        matrix C = mul(temp, temp);
+        return C;
+    }
+    else {
+        matrix C = mul(mul(temp, temp), A);
+        return C;
+    }
+}
+
+int main() {
+    matrix A;
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<2;j++) {
+            cin>>A.mat[i][j];
+        }
+    }
+    int n;cin>>n;
+
+    matrix C = power(A, n);
+    
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<2;j++) {
+            cout<<C.mat[i][j]<<' ';
+        }
+        cout<<endl;
+    }
+
+}
+ 
