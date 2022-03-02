67ea71d3d6cea0e0513842f98570a0d553f51847
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2022-03-02 14:22:06 +0600

    Update BE_iterative.cpp

--- Math/Binary Exponentiation/BE_iterative.cpp
+++ Math/Binary Exponentiation/BE_iterative.cpp
@@ -1,22 +1,21 @@
-//iterative approach
-// it is way faster than recursive approach
+/*
+* Author: BrownFalcon
+*/
 
-//time complexity = O(logb)
+// Time Complexity O(log n)
+// Space Complexity O(1)
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int power(int a, int b) {
-    int result = 1;
-    while(b) {
-        if(b%2) result *=a;
-        a *=a;
-        b /=2;
-    }
-    return result;
-}
-
 int main() {
-    int a, b;cin>>a>>b;
-    cout<<power(a,b)<<endl;
+    long long a, n;cin>>a>>n;
+    // determine power(a,n)
+    long long ans = 1;
+    while(n) {
+        if(n%2) ans*=a;
+        n = n >> 1;
+        a = a*a;
+    }
+    cout<<ans<<endl;
 }
