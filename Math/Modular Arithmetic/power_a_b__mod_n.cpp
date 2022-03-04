4cd844822d457d0778ff15970bdf49e854f57bf6
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2022-03-04 14:30:00 +0600

    Create power_a_b__mod_n.cpp

---
+++ Math/Modular Arithmetic/power_a_b__mod_n.cpp
@@ -0,0 +1,28 @@
+/*
+* Author: BrownFalcon
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const ll mod = 1e9 + 7; 
+
+ll power(ll a, ll b) {
+    ll ans = 1;
+    while(b) {
+        if(b%2) {
+            ans= (a*ans) % mod;
+        }
+        b = b >> 1;
+        a = (a*a) % mod;
+    }
+    return ans;
+}
+
+int main() {
+    ll a, b;cin>>a>>b;
+    cout<<power(a%mod, b%mod)<<endl;
+
+    return 0;
+}
